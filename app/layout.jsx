4cf5d142c07f4501b44e321c59a9e35f19e46259
issamaya <issam.bouzaffour@outlook.fr>
2024-01-23 23:40:17 -0500

    ajout normalize.css

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Inter } from 'next/font/google';
 import '@/style/globals.css';
+import 'normalize.css/normalize.css';
 import styles from './layout.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
