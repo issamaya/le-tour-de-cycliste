c0910b6dabc313c80b6148d3ad248adc24fa3220
ouldbouya <ouldbouyanizar@gmail.com>
2024-03-20 01:53:11 -0400

    finaliser le projet

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,6 +3,6 @@ import styles from './Footer.module.css'
 
 export default function footer () {
     return <footer className={styles.footer}>
-        copyright@college La Cite - devloppé par : issam bouzaffour
+        copyright@college La Cite - devloppé par : Issam Bouzaffour-Nizar Ouldbouya-Marouane Eloufir
     </footer>
 }
--- components/MenuNav.jsx
+++ components/MenuNav.jsx
@@ -4,9 +4,9 @@ import styles from './MenuNav.module.css'
 export default function MenuNav() {
     return <nav className={styles.nav}>
         <ul>   
-                    <li><Link href="/Ottawa" className={styles.lien} >Ottawa</Link></li>
-                    <li><Link href="/Montreal"  className={styles.lien}>Montreal</Link></li>
-                    <li><Link href="/Formulaire" className={styles.lien} >Contact</Link></li>
+            <li><Link href="/Ottawa" className={styles.lien} >Ottawa</Link></li>
+            <li><Link href="/Montreal"  className={styles.lien}>Montreal</Link></li>
+            <li><Link href="/Formulaire" className={styles.lien} >Contact</Link></li>
         </ul>
     </nav>
 }
